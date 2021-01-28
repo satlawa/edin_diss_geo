eb7da7f45b52403d93904f6234286f4061ab0b6c
satlawa <p.satlawa@gmx.net>
2021-01-28 16:38:01 +0100

    creating tiles

---
+++ new/DataTiles.py
@@ -0,0 +1,109 @@
+'''
+create one raster DTM (.tif) out of many tiles
+
+todo:
+* check if coordinate system is the same
+
+* write algorithm to merge pairs together
+'''
+
+from osgeo import gdal
+import pandas as pd
+import geopandas
+import os, sys, gdal
+import numpy as np
+
+class DataTiles(object):
+
+    def __init__(self, path_grid, path_out_dir):
+
+        # check if output path is valid
+        if os.path.exists(path_out_dir):
+            print("provided output path found")
+            # check if path contains / at the end
+            if path_out_dir[-1] == '/':
+                self.path_out_dir = path_out_dir
+            else:
+                self.path_out_dir = path_out_dir + '/'
+        else:
+            print("provided output path does not exist!")
+
+        # check if grid file extists
+        if os.path.isfile(path_grid):
+            print("provided grid file found")
+            self.grid = geopandas.read_file(path_grid)
+            self.grid_cut = self.grid
+        else:
+            print("provided grid file does not exist!")
+
+
+    def get_extend(self, raster_path):
+        '''
+        Get raster's extend
+        '''
+        src = gdal.Open(file_path)
+        ulx, xres, xskew, uly, yskew, yres  = src.GetGeoTransform()
+        lrx = ulx + (src.RasterXSize * xres)
+        lry = uly + (src.RasterYSize * yres)
+        return(ulx,uly,lrx,lry)
+
+
+    def clip_grid(self, extend=None ,raster_path=None):
+        '''
+        Clips grid to extend
+        '''
+        if extend:
+            ulx,uly,lrx,lry = extend
+        elif raster_path:
+            ulx,uly,lrx,lry = self.get_extend(path_raster)
+
+        self.grid_cut = self.grid[(ulx <= self.grid['left']) &
+                           (uly >= self.grid['top']) &
+                           (lrx >= self.grid['right']) &
+                           (lry <= self.grid['bottom'])]
+
+
+    def make_tiles(self, path_raster, typ, cores=1):
+        '''
+        Utalizes provided grid to clip raster into tiles.
+        Input:
+            raster(str): path to raster
+            type(str): 'ortho' | 'dsm' | 'dtm' | 'slope'
+        Return:
+            raster_tile
+        '''
+        if not os.path.isfile(path_raster):
+            print("provided raster file does not exist!")
+
+        elif typ not in {'ortho', 'dsm', 'dtm', 'slope'}:
+            print("provided type does not exist!")
+            print("please use one of these types:")
+            print("   'ortho' | 'dsm' | 'dtm' | 'slope'")
+
+            if not os.path.exists(os.path.join(path_out_dir, typ)):
+                os.mkdir(os.path.join(path_out_dir, typ))
+
+        else:
+            print("=========================================")
+            print("starting creation of tiles")
+            # get indices
+            ids = self.grid_cut['id'].to_numpy()
+            i = 0
+            # loop over all records
+            for index in ids:
+                if i % (ids.size // 10) == 0:
+                    os.system( 'cls' )
+                    print("Progress: {}%".format(i))
+                # get extend
+                extend = self.grid_cut.loc[index,'geometry'].bounds
+                # construct path of output file
+                path_out = self.path_out_dir + "/{}/tile_{}_{}.tif".format(typ, typ, index)
+                # construct command
+                bashCommand = "gdal_translate -projwin {} {} {} {} -a_nodata 0.0 -of GTiff {} {}".format(extend[0], extend[3], extend[2], extend[1], path_raster, path_out)
+                # execute command
+                os.system(bashCommand)
+                # increment
+                i += 1
+
+            print("finished creation of tiles")
+            print("=========================================")
